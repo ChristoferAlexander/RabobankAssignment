f8d6d83b47e265893b8d147172afa3d36d145dd3
Alex <alexandros@splendo.com>
2022-05-09 21:02:31 +0200

    Adjustments to Display.kt composable

--- app/src/main/java/com/example/rabobankassignment/ui/compose/Display.kt
+++ app/src/main/java/com/example/rabobankassignment/ui/compose/Display.kt
@@ -1,24 +1,23 @@
-@file:OptIn(ExperimentalFoundationApi::class)
-
 package com.example.rabobankassignment.ui.compose
 
 import android.net.Uri
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Modifier.Companion
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,12 +27,12 @@ import androidx.navigation.navArgument
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.rabobankassignment.R
-import com.example.rabobankassignment.model.CsvResult
+import com.example.rabobankassignment.model.CsvNavResult
 import com.example.rabobankassignment.model.CsvResultNavType
 import com.example.rabobankassignment.parser.CsvRecord
 import com.example.rabobankassignment.parser.CsvRecordValue
+import com.example.rabobankassignment.parser.CsvRecordValue.Value.*
 import com.example.rabobankassignment.parser.RecordValueType
-import com.example.rabobankassignment.parser.Value.*
 import com.example.rabobankassignment.ui.nav.NavRoute
 import com.example.rabobankassignment.ui.nav.getOrThrow
 import com.example.rabobankassignment.ui.theme.*
@@ -50,17 +49,18 @@ object DisplayRoute : NavRoute<DisplayViewModel> {
     /**
      * Call this to get navigation path to [DisplayRoute] with args
      *
-     * TODO improve to more generalized
+     * TODO improve to more generalized approach
      */
-    fun getWithArgs(data: CsvResult): String {
+    fun getWithArgs(data: CsvNavResult): String {
         return route.replace(
             "{$KEY_DISPLAY_DATA}",
             Uri.encode(Gson().toJson(data))
         )
     }
 
+    @Throws(IllegalArgumentException::class)
     fun getData(savedStateHandle: SavedStateHandle) =
-        savedStateHandle.getOrThrow<CsvResult>(KEY_DISPLAY_DATA)
+        savedStateHandle.getOrThrow<CsvNavResult>(KEY_DISPLAY_DATA)
 
     override fun getArguments(): List<NamedNavArgument> =
         listOf(navArgument(KEY_DISPLAY_DATA) { type = CsvResultNavType() })
@@ -69,72 +69,94 @@ object DisplayRoute : NavRoute<DisplayViewModel> {
     override fun viewModel(): DisplayViewModel = hiltViewModel()
 
     @Composable
-    override fun Content(viewModel: DisplayViewModel) = LoadedScreen(viewModel.data)
+    override fun Content(viewModel: DisplayViewModel) = LoadedScreen(viewModel)
 }
 
 @Composable
-fun LoadedScreen(data: CsvResult) {
+fun LoadedScreen(viewModel: DisplayViewModel) {
+    BackHandler {
+       viewModel.navigateUp()
+    }
+    LoadedLayout(viewModel.data)
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LoadedLayout(data: CsvNavResult) {
+    // Not the best choice as it can not scroll horizontally
+    // lots of limitations on compose side for grid layouts
+    // needs a custom implementation on LazyRow and LazyColumn so it can show items without header
+    // right now it will do a best effort attempt
     LazyVerticalGrid(
-        cells = GridCells.Fixed(data.columns.size),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        cells = data.columns?.let { GridCells.Fixed(it.size) } ?: GridCells.Adaptive(80.dp),
+        state = rememberLazyListState(),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_tiny)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_tiny)),
+        modifier = Modifier.background(Color.Black)
     ) {
-        items(data.columns) { header ->
-            HeaderGridItem(header)
+        data.columns?.let {
+            items(it) { header ->
+                Box(modifier = Modifier.heightIn(50.dp).background(Color.White)) {
+                    HeaderGridItem(header)
+                }
+            }
         }
         items(data.records.map { it.elements }.flatten()) { record ->
-            RecordGridItem(record)
+            Box(modifier = Modifier.heightIn(80.dp).background(Color.White)) {
+                RecordGridItem(record)
+            }
         }
     }
 }
 
 @Composable
-fun HeaderGridItem(title: String) {
+fun BoxScope.HeaderGridItem(title: String) {
     Text(
+        modifier = Modifier.align(Alignment.Center),
         text = title,
         style = Typography.headerTextStyle
     )
 }
 
 @Composable
-fun RecordGridItem(csvRecordValue: CsvRecordValue) {
+fun BoxScope.RecordGridItem(csvRecordValue: CsvRecordValue, modifier: Modifier = Modifier) {
     when (val value = csvRecordValue.getValue()) {
-        is DateValue -> DateValueItem(value.dateValue)
-        is IntValue -> IntValueItem(value.intValue)
-        is StringValue -> StringValueItem(value.stringValue)
-        is UrlValue -> AvatarValueItem(thumbnailUrl = value.urlValue)
+        is DateValue -> DateValueItem(value.dateValue, modifier)
+        is IntValue -> IntValueItem(value.intValue, modifier)
+        is StringValue -> StringValueItem(value.stringValue, modifier)
+        is UrlValue -> AvatarValueItem(thumbnailUrl = value.urlValue, modifier)
     }
 }
 
 @Composable
-fun StringValueItem(value: String) {
+fun BoxScope.StringValueItem(value: String, modifier: Modifier = Modifier) {
     Text(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.align(Alignment.Center),
         text = value,
         style = Typography.stringTextStyle
     )
 }
 
 @Composable
-fun IntValueItem(value: Int) {
+fun BoxScope.IntValueItem(value: Int, modifier: Modifier) {
     Text(
-        modifier = Companion.fillMaxSize(),
+        modifier = modifier.align(Alignment.Center),
         text = value.toString(),
         style = Typography.intTextStyle
     )
 }
 
 @Composable
-fun DateValueItem(date: String) {
+fun BoxScope.DateValueItem(date: String, modifier: Modifier) {
     Text(
-        modifier = Companion.fillMaxSize(),
+        modifier = modifier.align(Alignment.Center),
         text = date,
         style = Typography.dateTextStyle
     )
 }
 
 @Composable
-fun AvatarValueItem(thumbnailUrl: String) {
+fun BoxScope.AvatarValueItem(thumbnailUrl: String, modifier: Modifier) {
     AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(thumbnailUrl)
@@ -143,14 +165,14 @@ fun AvatarValueItem(thumbnailUrl: String) {
         placeholder = painterResource(R.drawable.ic_launcher_foreground),
         contentDescription = "Avatar item thumbnail picture",
         contentScale = ContentScale.Fit,
-        modifier = Modifier.padding(8.dp)
+        modifier = modifier.size(80.dp).padding(dimensionResource(R.dimen.padding_tiny)).align(Alignment.Center)
     )
 }
 
 @Preview
 @Composable
 fun LoadedScreenPreview() {
-    val dummyData = CsvResult(
+    val dummyData = CsvNavResult(
         columns = listOf("Column 1", "Long column 2", "Column 3", "Long column 4", "Long column 5"),
         records = listOf(
             CsvRecord(
@@ -164,5 +186,5 @@ fun LoadedScreenPreview() {
             )
         )
     )
-    LoadedScreen(dummyData)
+    LoadedLayout(dummyData)
 }
