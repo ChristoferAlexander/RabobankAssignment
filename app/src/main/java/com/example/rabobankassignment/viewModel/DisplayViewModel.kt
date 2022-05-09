226c416aa7f84f357629ae74e6243de26005e327
Alex <alexandros@splendo.com>
2022-05-09 20:57:03 +0200

    improved testability of DisplayViewModel.kt

--- app/src/main/java/com/example/rabobankassignment/viewModel/DisplayViewModel.kt
+++ app/src/main/java/com/example/rabobankassignment/viewModel/DisplayViewModel.kt
@@ -2,17 +2,18 @@ package com.example.rabobankassignment.viewModel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.rabobankassignment.model.CsvNavResult
 import com.example.rabobankassignment.ui.compose.DisplayRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import com.example.rabobankassignment.ui.nav.RouteNavigator
 import javax.inject.Inject
 
 @HiltViewModel
-class DisplayViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
+class DisplayViewModel constructor(
+    val data: CsvNavResult,
     private val routeNavigator: RouteNavigator
 ) : ViewModel(), RouteNavigator by routeNavigator {
 
-     val data = DisplayRoute.getData(savedStateHandle)
+    @Inject constructor(savedStateHandle: SavedStateHandle, routeNavigator: RouteNavigator) : this(DisplayRoute.getData(savedStateHandle), routeNavigator)
 
 }
