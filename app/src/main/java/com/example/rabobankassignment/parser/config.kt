1af52632c0ce40e5d779fca422d1bab8f5a743cd
Alex <alexandros@splendo.com>
2022-05-09 21:04:13 +0200

    improved config settings

--- app/src/main/java/com/example/rabobankassignment/parser/config.kt
+++ app/src/main/java/com/example/rabobankassignment/parser/config.kt
@@ -3,11 +3,18 @@ package com.example.rabobankassignment.parser
 import java.io.InputStream
 import java.nio.charset.Charset
 
+/**
+ * Contains CVS as [InputStream] and CSV parsing options
+ */
 data class CsvSourceConfig (
     val stream: InputStream,
     val charset: Charset = Charsets.UTF_8,
     val separatorChar: Char = ',',
-    val quoteChar: Char = '"'
+    val hasHeader:Boolean = true,
+    val quoteChar: Char = '"',
+    val dateFormatter: CsvDateFormatter = CsvDateFormatter()
 )
+
+data class CsvDateFormatter(val inputFormat: String = INPUT_DATE_FORMAT, val outputFormat: String = OUTPUT_DATE_FORMAT)
 
 fun CsvSourceConfig.bufferedReader() = stream.bufferedReader(charset)
